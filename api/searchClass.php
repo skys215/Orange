e65f9a1158cb15288a55feb937452a7a7b1297fd
skys215 <skys215@qq.com>
2016-04-04 23:10:04 +0800

    Return courseTime

--- api/searchClass.php
+++ api/searchClass.php
@@ -3,7 +3,7 @@
 	function searchClass( $keyword ){
 		global $_mysqli;
 
-		$sql = 'SELECT cid, classname, classroom, classteacher, mainclass, bz, college FROM courseinfo3 WHERE term='.TERM.' AND (cid LIKE \''.$keyword.'%\' OR classname LIKE \'%'.$keyword.'%\' OR classroom LIKE \'%'.$keyword.'%\' OR classteacher LIKE \'%'.$keyword.'%\' OR mainclass LIKE \'%'.$keyword.'%\' OR bz LIKE \'%'.$keyword.'%\') LIMIT 10';
+		$sql = 'SELECT cid, classname, classroom, classteacher, mainclass, bz, college, courseTimeSing, courseTimeDoub, courseTimeNght FROM courseinfo3 WHERE term='.TERM.' AND (cid LIKE \''.$keyword.'%\' OR classname LIKE \'%'.$keyword.'%\' OR classroom LIKE \'%'.$keyword.'%\' OR classteacher LIKE \'%'.$keyword.'%\' OR mainclass LIKE \'%'.$keyword.'%\' OR bz LIKE \'%'.$keyword.'%\') LIMIT 10';
 
 		$query = $_mysqli->query( $sql );
 
